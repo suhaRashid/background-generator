4665563dc0bc59b45282fc3d9ce4f342205c0a28
Suha Rashid <suha.nubi@gmail.com>
2018-07-05 01:24:36 +0530

    commented out

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
-
+// lets have a check with the git
 function setGradient() {
 	body.style.background = 
 	"linear-gradient(to right, " 
